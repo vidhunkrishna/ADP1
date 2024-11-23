fd9cf84c50240983b0de84204520a454a5360cb2
vidhunkrishna <vidhunkrishna903@gmail.com>
2024-11-23 09:45:27 +0530

    Add files via upload

---
+++ html/int.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string a;
+    cin>>a;
+    cout<<"Hello "<<a<<"! Welcome to Examly Event Management System";
+}
---
+++ html/kka.js
@@ -0,0 +1,10 @@
+function add(a,b){
+    var c;
+    c=a+b;
+    window.alert("sum of "+ a +" and "+ b + " is "+c);
+    window.confirm(c);
+}
+var a,b;
+    a=parseInt(prompt("enter a value"));
+    b=parseInt(prompt("enter b value "));
+add(a,b)
